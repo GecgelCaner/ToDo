6a4280a0fd258acd7eb09ea291b9bed6345ba2c1
GecgelCaner <gecgelcaner.10@gmail.com>
2023-07-13 11:37:31 +0300

    todos__text--done button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Todos from "./components/Todos";
-
 import TodoField from "./components/TodoField";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      name: "Selam",
+      status: false,
+    },
+    {
+      id: 2,
+      name: "Naber",
+      status: true,
+    },
+  ]);
   return (
     <div className="App">
       <h1>todos</h1>
       <TodoField />
-      <Todos />
+      <Todos todos={todos} />
     </div>
   );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,45 +1,27 @@
 import React from "react";
 
-const Todos = () => {
-  return (
+const Todos = ({ todos }) => {
+  return todos.length > 0 ? (
     <ul className="todos">
-      <li className="todos__item">
-        <span className="todos_text">GYM</span>
-        <div className="todos_buttons">
-          <a href="#">❌</a>
-          <a href="#">✅</a>
-        </div>
-      </li>
-      <li className="todos__item">
-        <span className="todos_text">GYM</span>
-        <div className="todos_buttons">
-          <a href="#">❌</a>
-          <a href="#">✅</a>
-        </div>
-      </li>
-      <li className="todos__item">
-        <span className="todos_text">GYM</span>
-        <div className="todos_buttons">
-          <a href="#">❌</a>
-          <a href="#">✅</a>
-        </div>
-      </li>
-      <li className="todos__item">
-        <span className="todos_text">GYM</span>
-        <div className="todos_buttons">
-          <a href="#">❌</a>
-          <a href="#">✅</a>
-        </div>
-      </li>
-      <li className="todos__item">
-        <span className="todos_text">GYM</span>
-        <div className="todos_buttons">
-          <a href="#">❌</a>
-          <a href="#">✅</a>
-        </div>
-      </li>
+      {todos.map((todo) => (
+        <li key={todo.id} className="todos__item">
+          <span
+            className={
+              todo.status ? "todos_text  todos__text--done" : "todos_text"
+            }
+          >
+            {todo.name}
+          </span>
+          <div className="todos_buttons">
+            <button href="#">❌</button>
+            <button disabled={todo.status} href="#">
+              ✅
+            </button>
+          </div>
+        </li>
+      ))}
     </ul>
-  );
+  ) : null;
 };
 
 export default Todos;
