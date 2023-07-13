6a94b856022a706663e112afdfd69d756aec40fd
GecgelCaner <gecgelcaner.10@gmail.com>
2023-07-13 17:31:27 +0300

    inputRef

--- src/components/TodoField.js
+++ src/components/TodoField.js
@@ -1,27 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const TodoField = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    addTodo({
+      id: uuidv4(),
+      name: value,
+      status: false,
+    });
+    setValue("");
+    inputRef.current.focus();
+  };
+
   return (
     <div className="todoField">
       <input
+        ref={inputRef}
         value={value}
-        onChange={(e) => setValue(e.target.value)}  
+        onChange={(e) => setValue(e.target.value)}
         type="text"
         className="todoField__input"
       />
-      <button
-        onClick={() =>
-          addTodo({
-            id: uuidv4(),
-            name: value,
-            status: false,
-          })
-        }
-        className="todoField__btn"
-      >
+      <button onClick={() => handleClick()} className="todoField__btn">
         Add
       </button>
     </div>
